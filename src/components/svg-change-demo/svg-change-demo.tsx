0775c78ddb5d0d6d8e44a028b7d5c03792517af8
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-11-05 11:05:07 +0600

    Added ability to use uploaded image

--- src/components/svg-change-demo/svg-change-demo.tsx
+++ src/components/svg-change-demo/svg-change-demo.tsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Heading, Image, Button, Stack, Tooltip } from "@chakra-ui/core";
 import { Props } from "./svg-change-demo.model";
 import { css, jsx } from "@emotion/core";
 
 const SVGChangeDemo: React.FC<Props> = (props: Props) => {
   const { cssFilterValue } = props;
+  const [ImgURL, setImgURL] = useState("/example-demo.svg");
+
+  const handleSvgChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setImgURL(URL.createObjectURL(e.target.files[0]));
+  };
+
   return (
     <Box m="0 auto" width="500px" mt={16}>
       <Stack isInline>
         <Heading flex={1} as="h4" size="md" mb={5}>
-          Filter applied to SVG
+          Filter applied to Image (SVG preferred)
         </Heading>
         <Tooltip label="upload custom svg" aria-label="upload custom svg">
           <label htmlFor="upload-svg" id="upload-svg-label">
@@ -17,15 +23,23 @@ const SVGChangeDemo: React.FC<Props> = (props: Props) => {
           </label>
         </Tooltip>
 
-        <input type="file" accept="image/*" name="svg" id="upload-svg" />
+        <input
+          type="file"
+          accept="image/*"
+          name="svg"
+          id="upload-svg"
+          onChange={handleSvgChange}
+        />
       </Stack>
 
       <Image
-        src="/example-demo.svg"
+        src={ImgURL ? ImgURL : "/example-demo.svg"}
         alt="example svg"
         css={css`
           ${cssFilterValue}
         `}
+        maxHeight="350px"
+        maxWidth="350px"
       />
     </Box>
   );
